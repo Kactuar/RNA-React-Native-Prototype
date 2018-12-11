46d62553a6af6eac5e810ced605361964414c56c
AlexeyS <alexey.stepanov@deleteagency.com>
2018-12-11 13:09:05 +0500

    Styling tabs

--- src/general/colors.js
+++ src/general/colors.js
@@ -2,7 +2,8 @@ const colors = {
     purple: '#1a1c3c',
     blue: '#0160af',
     mystic: '#eaf2f6',
-    yellow: '#ffb500'
+    yellow: '#ffb500',
+    frenchGrey: '#b8b8c2'
 };
 
 export default colors;
--- src/navigation/tab-main.js
+++ src/navigation/tab-main.js
@@ -1,27 +1,73 @@
 import React from 'react';
-import { createBottomTabNavigator } from 'react-navigation';
-import HomeScreen from 'rootSrc/screens/home-screen';
+import { createBottomTabNavigator  } from 'react-navigation';
 import StackTrack from './stack-track';
 import StackWatch from './stack-watch';
 import StackScore from './stack-score';
 import StackCommentary from './stack-commentary';
-
+import IconScore from 'rootSrc/general/svg-components/icon-score';
+import IconComment from 'rootSrc/general/svg-components/icon-comment';
+import IconVideo from 'rootSrc/general/svg-components/icon-video';
+import IconPin from 'rootSrc/general/svg-components/icon-pin';
+import colors from 'rootSrc/general/colors';
 
 
 
 const TabMain = createBottomTabNavigator(
     {
-        Commentary: { screen: StackCommentary },
-        Score: { screen: StackScore },
-        Watch: { screen: StackWatch},
-        Track: { screen: StackTrack },
+        Commentary: {
+            screen: StackCommentary,
+            navigationOptions: {
+                title: "Commentary",
+                tabBarIcon: ({ tintColor }) => (
+                    <IconComment width={17} height={16} fill={tintColor} />
+                )
+            }
+        },
+        Score: {
+            screen: StackScore,
+            navigationOptions: {
+                title: "Score",
+                tabBarIcon: ({ tintColor }) => (
+                    <IconScore width={17} height={14} fill={tintColor} />
+                )
+            }
+        },
+        Watch: {
+            screen: StackWatch,
+            navigationOptions: {
+                title: "Watch",
+                tabBarIcon: ({ tintColor }) => (
+                    <IconVideo width={17} height={13} fill={tintColor} />
+                )
+            }
+        },
+        Track: {
+            screen: StackTrack,
+            navigationOptions: {
+                title: "Track",
+                tabBarIcon: ({ tintColor }) => (
+                    <IconPin width={13} height={20} fill={tintColor} />
+                )
+            }
+        },
 
 
     },
     {
+        swipeEnabled: true,
+        animationEnabled: true,
         tabBarOptions: {
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
+            position: 'bottom',
+            activeTintColor: colors.yellow,
+            inactiveTintColor: colors.frenchGrey,
+            style: {
+                backgroundColor: colors.purple,
+                paddingBottom: 2,
+                paddingTop: 2
+            },
+            labelStyle: {
+                fontFamily: 'roboto-regular'
+            }
         },
     }
 );
